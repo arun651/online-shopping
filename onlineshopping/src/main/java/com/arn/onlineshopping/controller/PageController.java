4c2a5276aa0b557f845d471fea38803f78459fd4
arun <arun651@gmail.com>
2018-08-03 15:17:03 +0530

    Static resources set up completed.

--- onlineshopping/src/main/java/com/arn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/arn/onlineshopping/controller/PageController.java
@@ -1,12 +1,13 @@
 package com.arn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	@RequestMapping("/")
+	@RequestMapping(value={"/", "/home", "/index"})
 	public ModelAndView index() {
 		
 		System.out.println("i m here");
@@ -14,4 +15,30 @@ public class PageController {
 		mv.addObject("greeting", "Welcome to Spring MVC");
 		return mv;
 	}
+	
+//	-- DEMO USING @RequestParam ANNOTATION
+//	@RequestMapping("/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting){
+//		System.out.println("i m here");
+//		if(greeting == null){
+//			greeting = "Hello there";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+
+//	-- DEMO USING @PathVariable ANNOTATION
+//	@RequestMapping("/test/{greeting}")
+//	public ModelAndView test(@PathVariable("greeting") String greeting){
+//		System.out.println("i m here");
+//		if(greeting == null){
+//			greeting = "Hello there";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+
+
 }
